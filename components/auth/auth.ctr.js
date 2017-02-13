240fe19adb7e70ca1788a1a7751cafab489cf1c1
Ryan Chenkie <ryanchenkie@gmail.com>
2017-02-13 00:34:46 -0500

    update auth factory

--- components/auth/auth.ctr.js
+++ components/auth/auth.ctr.js
@@ -15,13 +15,11 @@
       
       function createUser() {
         
-        // If there is already a user logged in,
-        // log them out before proceeding
-        auth.ref.$unauth();
-        
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(vm.email, vm.password)
+        auth
+          .$createUserWithEmailAndPassword(vm.email, vm.password)
+          .then(function(result) {
+            $state.go('classifieds');
+          })
           .catch(function(error) {
             vm.error = error;
         });
